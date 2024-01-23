a5248fee7b2e3176912df6015ff365ccfd1762bd
Richard Barbosa Silva <112967185+Richardbarbosasilva@users.noreply.github.com>
2024-01-23 16:01:45 -0400

    Create autoshutdown.ps1

---
+++ AUTOSHUTDOWN/autoshutdown.ps1
@@ -0,0 +1,5 @@
+
+
+Start-Process "\\172.25.200.3\clickip\cleanmgr\Python312\python.exe"  -ArgumentList "\\172.25.200.3\clickip\cleanmgr\Python312\autoshutdown.py" -WindowStyle Hidden -verb RunAs 
+
+exit
